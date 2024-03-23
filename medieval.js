247326fd7064324cefc84bdd101b3665b458d442
rayanne58379 <161958889+rayanne58379@users.noreply.github.com>
2024-03-23 13:52:38 -0300

    Update medieval.js

--- medieval.js
+++ medieval.js
@@ -4,8 +4,7 @@ let currentIndex = 0;
 function changeImage() {
   currentIndex = (currentIndex + 1) % 
   images.length;
-  document.getElementById("bannerImg")= images[currentIndex];
+  document.getElementById("bannerImg").src = images[currentIndex];
 }
-// Troca de imagem a cada 3 segundos (3000 milissegundos)
 setInterval(changeImage, 7000)
 
