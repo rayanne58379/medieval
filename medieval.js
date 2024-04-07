3ec73c346a09ee5ec8edf61eae8f15d192e4f909
rayanne58379 <161958889+rayanne58379@users.noreply.github.com>
2024-04-07 15:43:38 -0300

    Add files via upload

--- medieval.js
+++ medieval.js
@@ -1,4 +1,4 @@
-const images = ["banner1.webp", "banner22.webp"];
+const images = ["fotos/banner1.webp", "fotos/banner22.webp"];
 let currentIndex = 0;
 
 function changeImage() {
@@ -6,5 +6,6 @@ function changeImage() {
   images.length;
   document.getElementById("bannerImg").src = images[currentIndex];
 }
+// Troca de imagem a cada 3 segundos (3000 milissegundos)
 setInterval(changeImage, 7000)
 
